354b3216498473904ce1e338c000246cd53c5a64
jlga94 <jose.gil@pucp.pe>
2020-04-05 22:50:16 +0200

    line and weather consumers

--- consumers/models/weather.py
+++ consumers/models/weather.py
@@ -1,6 +1,6 @@
 """Contains functionality related to Weather"""
 import logging
-
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +15,7 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        data = json.loads(message.value())
+        self.temperature = data["temperature"]
+        self.status = data["status"]
+        logger.info("weather process_message was processed")
