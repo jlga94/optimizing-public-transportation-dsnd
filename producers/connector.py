ab11478eab8e512c30acb6f0c57972271913486b
jlga94 <jose.gil@pucp.pe>
2020-04-05 16:35:17 +0200

    fix postgres connector docker

--- producers/connector.py
+++ producers/connector.py
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
-CONNECTOR_NAME = "stations"
+CONNECTOR_NAME = "stations-jdbc"
 
 
 def configure_connector():
     """Starts and configures the Kafka Connect connector"""
-    logging.debug("creating or updating kafka connect connector...")
+    logging.info("creating or updating kafka connect connector...")
 
     resp = requests.get(f"{KAFKA_CONNECT_URL}/{CONNECTOR_NAME}")
     if resp.status_code == 200:
@@ -46,21 +46,14 @@ def configure_connector():
                "value.converter": "org.apache.kafka.connect.json.JsonConverter",
                "value.converter.schemas.enable": "false",
                "batch.max.rows": "500",
-               # TODO
-               "connection.url": "jdbc:postgresql://localhost:5432/cta",
-               # TODO
+               "connection.url": "jdbc:postgresql://postgres:5432/cta",  # Docker
+               # "connection.url": "jdbc:postgresql://localhost:5432/cta",
                "connection.user": "cta_admin",
-               # TODO
                "connection.password": "chicago",
-               # TODO
                "table.whitelist": "stations",
-               # TODO
                "mode": "incrementing",
-               # TODO
                "incrementing.column.name": "stop_id",
-               # TODO
                "topic.prefix": "stations-",
-               # TODO
                "poll.interval.ms": "60000",
            }
        }),
@@ -68,7 +61,7 @@ def configure_connector():
 
     ## Ensure a healthy response was given
     resp.raise_for_status()
-    logging.debug("connector created successfully")
+    logging.info("connector created successfully")
 
 
 if __name__ == "__main__":
--- producers/models/station.py
+++ producers/models/station.py
@@ -15,10 +15,6 @@ class Station(Producer):
     """Defines a single station"""
 
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
     value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
@@ -31,13 +27,7 @@ class Station(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"com.udacity.{station_name}" # TODO: Come up with a better topic name
+        topic_name = f"org.chicago.cta.station.arrivals.{station_name}" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
@@ -56,11 +46,7 @@ class Station(Producer):
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
+
         self.producer.produce(
            topic=self.topic_name,
            key={"timestamp": self.time_millis()},
@@ -71,7 +57,7 @@ class Station(Producer):
                "line": self.color.name,
                "train_status": train.status.name,
                "prev_station_id": prev_station_id or -1,
-               "prev_direction": prev_direction or ""
+               "prev_direction": prev_direction or "",
            },
         )
         logger.info("station message produced")
